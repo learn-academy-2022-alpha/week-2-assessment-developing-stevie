b39b1d93198de5831d37e08e5c22bc85aa36561b
developing-stevie <mitchellevie@icloud.com>
2022-03-09 22:07:51 -0500

    push week 2 changes

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -22,42 +22,74 @@ const numbersArray2 = [24, 27, 30, 33, 36]
 
 // b) Create the function that makes the test pass.
 
-// array1 concat array2 
-// console log arrays times 3
-// having a had time with jest and just code layout i thought doing the example may help but its not i really feel like i need to go over this more im not sure if i just dont know exactly what to do or if im just doubting myself i cant seem to commit to a way of laying out any of these codes i thought more words and the comment texts working it out in there would help but falling short 
-
-const arrays = numbersArray1.concat(numbersArray2)
-
-
-{
-    console.log(arrays * 3)
-}
 
 
 // --------------------1) Create a function that takes a number as an argument and decides if the number is evenly divisble by three or not.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const num1 = 15
-// Expected output: "15 is divisible by three"
-const num2 = 0
-// Expected output: "0 is divisible by three"
-const num3 = -7
-// Expected output: "-7 is not divisible by three"
+
+// a describe method that lists the name of the function OR naming of the particular test.
+describe("divide", () => { 
+
+    // a test/it method, nested within the describe block, that in plain words, describes that the function does.
+    it("takes a number as an argument and decides if the number is evenly divisble by three or not.", () => {
+  
+
+        const num1 = 15
+        // Expected output: "15 is divisible by three"
+        const num2 = 0
+        // Expected output: "0 is divisible by three"
+        const num3 = -7
+        // Expected output: "-7 is not divisible by three"
+
+
+      //an expect method, nested within the test block, calling on the hello() function, followed by the .toEqual() matcher that checks the expected output of the function return.
+      expect(divide(num1)).toEqual("15 is divisible by three")
+      expect(divide(num2)).toEqual("0 is divisible by three")
+      expect(divide(num3)).toEqual("-7 not divisible by three")
+    })
+  })
+//good failure
+// FAIL  ./code-challenges.test.js
+// divide
+// ✕ takes a number as an argument and decides if the number is evenly divisble by three or not. (1 ms)
+
+// ● divide › takes a number as an argument and decides if the number is evenly divisble by three or not.
+
+// ReferenceError: divide is not defined
+
 
 
 // b) Create the function that makes the test pass.
+//it passed
+// make a function named divide
+// looks at a paramater (number)
+// use modulo operator to determine
+// if use string interpolation `${number}` is divisable by 3 
 
-// if (num1) is divisible three 
-// then return "(numb1) is divisable by three"
-// if (num2) is divisible three
-// then return "(numb2) is divisable by three"
-// if (num3) is divisible three 
-// then return "(numb3) is divisable by three"
 
-const if (numb1) is % 3  { return ""
 
+const divide = (number) => {
+    if(number % 3 === 0) {
+        return `${number} is divisible by three`
+    } else {
+        return `${number} not divisible by three`
+    }
 }
+
+//   divide
+//     ✓ takes a number as an argument and decides if the number is evenly divisble by three or not. (5 ms)
+
+// Test Suites: 1 passed, 1 total
+// Tests:       1 passed, 1 total
+
+
+//------------notes
+//if() {}
+//else{}
+//const name = (number) => {return}
+
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
 // a) Create a test with expect statements for each of the variables provided.
@@ -67,26 +99,46 @@ const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
 const randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
 // Expected output: ["Temperature", "Database", "Chopsticks", "Mango", "Deduction"]
 
-// randomnouns1 concat randomnous2 equals words
-// words equals lotswords split
-// words map create function
-// return word index of zero touppercase
-// join
 
-var words = randomNouns1.concat(randomNouns2)
-var lotswords = words.split()
+// a describe method that lists the name of the function OR naming of the particular test.
+describe("upper", () => {
 
-words.map() => {
-    return [0].toUpperCase().join(" ");
+    // a test/it method, nested within the describe block, that in plain words, describes that the function does.
+    it("that takes in an array of words and returns an array with all the words capitalized.", () => {
+  
+      //an expect method, nested within the test block, calling on the hello() function, followed by the .toEqual() matcher that checks the expected output of the function return.
+      expect(upper(randomNouns1)).toEqual(["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"])
+      expect(upper(randomNouns2)).toEqual(["Temperature", "Database", "Chopsticks", "Mango", "Deduction"])
+    })
+  })
+
+// good fail
+//   Test Suites: 1 failed, 1 total
+// Tests:       1 failed, 1 passed, 2 total
+// b) Create the function that makes the test pass.
+
+// name function upper
+//takes in paramater array 
+// returns an array of the same length
+//with the first letter capitolized
+//i need to find something that will allow me to split this word
+// Test Suites: 1 passed, 1 total
+// Tests:       2 passed, 2 total
+
+const upper = (array) => { 
+    
+    return array.map(value => {
+        return value[0].toUpperCase() + value.substring(1)
+    })
 }
 
 
 
-// b) Create the function that makes the test pass.
-
 
 
 // --------------------3) Create a function that takes in a string and logs the index of the first vowel.
+
+
 
 // a) Create a test with expect statements for each of the variables provided.
 
@@ -101,11 +153,38 @@ const vowelTester3 = "challenges"
 // b) Create the function that makes the test pass.
 
 
-// ive been thinking about how to finish this code but java is getting to me like all those jumpstart people today bash my head a few times on the keyboard and sleep maybe the code will work its way into this test by osmosis jones
-// the only way i can see this coming together is taking all the consts and running them through a loop that tells you if the vowel is in the string or not then running that through an indexof or a less than greater than index of loop in a function but i'm having a lot of trouble thinking of how even to begin that 
+// a describe method that lists the name of the function OR naming of the particular test.
+describe("vowelIndex", () => {
+
+    // a test/it method, nested within the describe block, that in plain words, describes that the function does.
+    it("that takes in a string and logs the index of the first vowel.", () => {
+  
+      //an expect method, nested within the test block, calling on the hello() function, followed by the .toEqual() matcher that checks the expected output of the function return.
+      expect(vowelIndex(vowelTester1)).toEqual(1)
+      expect(vowelIndex(vowelTester2)).toEqual(0)
+      expect(vowelIndex(vowelTester3)).toEqual(2)
+    })
+  })
 
 
-if (vowelTester1.includes("e"))
-(vowelTester2.includes("a"))
-(vowelTester3.includes("a"))
-console.log (.indexof(" "))
+   const vowelIndex = (string) => { 
+    let vowel = (["A","a","E","U","e","u","i","I","O","o"])
+    for (let i=0; i<string.length; i++){
+        if (vowel.includes(string[i])=== true) {
+        return (i)
+    }
+
+   }
+   }
+
+
+//    PASS  ./code-challenges.test.js
+//    divide
+//      ✓ takes a number as an argument and decides if the number is evenly divisble by three or not. (5 ms)
+//    upper
+//      ✓ that takes in an array of words and returns an array with all the words capitalized. (2 ms)
+//    vowelIndex
+//      ✓ that takes in a string and logs the index of the first vowel. (1 ms)
+ 
+//  Test Suites: 1 passed, 1 total
+//  Tests:       3 passed, 3 total
